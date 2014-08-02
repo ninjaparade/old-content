1f3ca03891fa7b3205fdc518e2facb2ac03ea1fc
Ninjaparade <ninja@ninjaparade.ca>
2014-08-01 19:23:44 -0700

    updated posts table lang file

--- lang/en/posts/table.php
+++ lang/en/posts/table.php
@@ -2,16 +2,18 @@
 
 return [
 
-	'id' => 'Id',
-	'author_id' => 'Author_Id',
-	'slug' => 'Slug',
-	'title' => 'Title',
-	'blog' => 'Blog',
+	'id'             => 'Id',
+	'author_id'      => 'Author',
+	'tags'           => 'Tagged',
+	'category'       => 'Published Under',
+	'slug'           => 'Slug',
+	'title'          => 'Title',
+	'blog'           => 'Blog',
 	'publish_status' => 'Publish_Status',
-	'created_at' => 'Created At',
-	'post_type' => 'Post_Type',
-	'pullquote' => 'Pullquote',
-	'content' => 'Content',
-	'private' => 'Private',
+	'created_at'     => 'Created At',
+	'post_type'      => 'Post_Type',
+	'pullquote'      => 'Pullquote',
+	'content'        => 'Content',
+	'private'        => 'Private',
 
 ];
