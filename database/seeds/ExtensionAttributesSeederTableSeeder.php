feef7892d0290af23ba112a00a61282209162e8e
Ninjaparade <ninja@ninjaparade.ca>
2014-08-10 17:20:42 -0700

    added seeder
    Signed-off-by: Ninjaparade <ninja@ninjaparade.ca>

---
+++ database/seeds/ExtensionAttributesSeederTableSeeder.php
@@ -0,0 +1,18 @@
+<?php namespace Ninjaparade\Content\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+
+class ExtensionAttributesSeederTableSeeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$attribute = app('Platform\Attributes\Repositories\AttributeRepositoryInterface');
+
+	}
+
+}
