c8b6e27e145bdde105af036b727388432f5f5b94
Ninjaparade <ninja@ninjaparade.ca>
2014-08-12 11:36:52 -0700

    added front end controller methods

--- src/Controllers/Frontend/PostsController.php
+++ src/Controllers/Frontend/PostsController.php
@@ -3,8 +3,37 @@
 use Platform\Foundation\Controllers\BaseController;
 use View;
 
+use Ninjaparade\Content\Repositories\PostRepositoryInterface;
+
 class PostsController extends BaseController {
 
+	public function __construct( PostRepositoryInterface $post)
+	{
+		parent::__construct();
+
+		$this->post = $post;
+	}
+
+	public function posts($posttype)
+	{
+		$view = $this->getView($posttype. '-index');
+
+		$posts = $this->post->byPostType($posttype);
+
+		return View::make($view)->with(compact('posts'));
+	}
+
+	public function single($posttype, $slug)
+	{
+		$view = $this->getView($posttype. '-single');
+
+		$post = $this->post->bySlug($slug);
+
+
+		
+
+		return View::make($view)->with(compact('post'));
+	}
 	/**
 	 * Return the main view.
 	 *
@@ -13,6 +42,11 @@ class PostsController extends BaseController {
 	public function index()
 	{
 		return View::make('ninjaparade/content::index');
+	}
+
+	protected function getView($view)
+	{
+		 return "ninjaparade/content::{$view}";
 	}
 
 }
