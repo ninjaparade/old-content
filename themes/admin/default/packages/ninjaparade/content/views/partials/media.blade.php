360a30f6451bb2dad3104d356c4b8896420708e4
Ninjaparade <ninja@ninjaparade.ca>
2014-08-27 22:11:54 -0700

    updated media partial
    Signed-off-by: Ninjaparade <ninja@ninjaparade.ca>

--- themes/admin/default/packages/ninjaparade/content/views/partials/media.blade.php
+++ themes/admin/default/packages/ninjaparade/content/views/partials/media.blade.php
@@ -1,10 +1,14 @@
-<div class="col-sm-6 col-md-4">
+<div class="col-sm-6 col-md-4" id="media-{{$media->id}}">
     <div class="thumbnail">
-      <img src="@media($media['id'])" alt="...">
+      <img src="@media($media->id)" alt="...">
       <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        <p><span class="label label-default">{{$media->id}}</span></p>
+        <p>{{$media->name}}</p>
+        <p>
+        	
+			<button type="submit" class="btn btn-danger delete-image" data-media-id="{{ $media->id }}">Delete</button>	
+        	
+        <a href="#" class="btn btn-default" role="button">Button</a></p>
       </div>
     </div>
   </div>
