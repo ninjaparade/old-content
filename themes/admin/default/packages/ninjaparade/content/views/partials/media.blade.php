9c6b199d30119c665042e254993fbe2abcd00dcb
Ninjaparade <ninja@ninjaparade.ca>
2014-08-28 20:11:21 -0700

    updated media partial
    Signed-off-by: Ninjaparade <ninja@ninjaparade.ca>

--- themes/admin/default/packages/ninjaparade/content/views/partials/media.blade.php
+++ themes/admin/default/packages/ninjaparade/content/views/partials/media.blade.php
@@ -1,14 +1,12 @@
-<div class="col-sm-6 col-md-4" id="media-{{$media->id}}">
+<div class="col-sm-6 col-md-4" id="media-{{$media->id}}" data-upload-image="{{$media->id}}">
     <div class="thumbnail">
       <img src="@media($media->id)" alt="...">
       <div class="caption">
         <p><span class="label label-default">{{$media->id}}</span></p>
-        <p>{{$media->name}}</p>
         <p>
-        	
-			<button type="submit" class="btn btn-danger delete-image" data-media-id="{{ $media->id }}">Delete</button>	
-        	
-        <a href="#" class="btn btn-default" role="button">Button</a></p>
+        	<button type="submit" class="btn btn-danger delete-image" data-media-id="{{ $media->id }}">Delete</button>	
+          <button class="btn btn-default cover-image" role="button" data-media-id="{{ $media->id }}" data-toggle="button">Set Cover Image</button>
+        </p>
       </div>
     </div>
   </div>
